393bb8eb3c5355c7e9aa1096c5212b0efa4ee9a8
whuffman <whuffman91@gmail.com>
2015-02-06 10:57:47 -0800

    Added exponential moving average to train_mini in neural_net.py.  Not tested

--- neural_net.py
+++ neural_net.py
@@ -20,7 +20,7 @@ class Neural_Net(Net):
             newindex = newindex - data.shape[1]
         return np.concatenate((data[:,startindex:], data[:,:newindex]),1), newindex
 
-    def train_mini(self, data, labels, mbsize, epochs, tag='', taginc=100, valid_data=None, valid_labels=None):
+    def train_mini(self, data, labels, mbsize, epochs, tag='', taginc=100, valid_data=None, valid_labels=None,ema_alpha=0.3):
         self.set_buffer_depth(mbsize)
         rcerror = []
         index = 0
@@ -28,19 +28,25 @@ class Neural_Net(Net):
         vindex = 0
         validation = (valid_data is not None) and (valid_labels is not None)
         lim = data.shape[1]*epochs/float(mbsize)
+        tma = 1
+        valma = 1
         for y in xrange(int(lim)):
             mbdata, phony = self.make_mini(data, index, mbsize)
             mblabels, index = self.make_mini(labels, index, mbsize)
             self.backprop(mbdata, mblabels)
             rcerror.append(self.cost(mbdata,mblabels))
             if (y % taginc) == taginc-1:
+                newt = sum(rcerror[-taginc:])/float(taginc*mbsize)
+                tma = ema_alpha*newt + (1-ema_alpha)*tma
                 if not validation:
-                    print tag+str(y),100*round(y/lim,4),sum(rcerror[-taginc:])/(float(taginc)*float(mbsize))
+                    print tag+str(y),100*round(y/lim,4),newt,tma
                 else:
                     vdata, phony = self.make_mini(valid_data, vindex, mbsize)
                     vlabels, vindex = self.make_mini(valid_labels, vindex, mbsize)
                     verror.append(self.cost(vdata, vlabels))
-                    print tag+str(y),100*round(y/lim,4),sum(rcerror[-taginc:])/(float(taginc)*float(mbsize)), verror[-1]/float(mbsize)
+                    newv = verror[-1]/float(mbsize)
+                    valma = ema_alpha*newv + (1-ema_alpha)*valma
+                    print tag+str(y),100*round(y/lim,4),newt,newv,tma,valma
         return rcerror, verror
     
     def backprop(self, data, target):
